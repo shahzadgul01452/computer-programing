52f843faeaebf58405c0ba645886e60f3df19a0c
shahzadgul01452 <gulshazad169@gmail.com>
2026-04-21 19:59:00 -0700

    Add files via upload

---
+++ lab 6/LAB 6 TASK/Multiplication Table.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "Multiplication Table (1 to 10)" << endl;
+    cout << "================================" << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << setw(4) << i * j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
